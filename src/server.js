941a808e4b8e422a7ee1fb8347a6edf19cb2cb04
Renan Guedes <renanguedes.br@gmail.com>
2020-05-11 23:00:49 -0300

    object knex test

--- src/server.js
+++ src/server.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const app = express()
 
+const knex = require('./database')
+
 const url = new URL('http://127.0.0.1:8085/')
 
-app.get('/users', (req, res) => {
+app.use(express.json())
+
+app.get('/users', async (req, res) => {
+  
+  const allUsers = await knex('users')
   res.status(200)
-  console.log('OK')
-  return res.send('Hello!')
+  
+  return res.json(allUsers)
+})
+
+app.post('/users', async (req, res) => {
+  const { username } = req.body
+
+  const newUser = await knex('users').insert({ username })
+
+  console.log(username)
+  return res.json(newUser)
 })
 
 app.listen(url.port, () => console.log(`Server is running on ${url.href}`))
